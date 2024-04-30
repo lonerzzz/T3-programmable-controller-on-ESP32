29da5cd3c423ae5d512cd1e04d8aafd0c22d842c
Jason K <jason.kania@ymail.com>
2024-04-29 23:37:19 -0400

    Update outputs.c

--- temco_IO_control/src/outputs.c
+++ temco_IO_control/src/outputs.c
@@ -1,106 +1,101 @@
 #include "controls.h"
 
-
 #ifdef OUTPUT
 
-extern Str_out_point far	outputs[MAX_OUTS];
+extern Str_out_point far	outputs[MAX__outputs];
 
-//U8_T far control_auto[MAX_OUTS];
-//U8_T far switch_status_back[MAX_OUTS];
+//uint8_t far control_auto[MAX__outputs];
+//uint8_t far switch_status_back[MAX__outputs];
 
 void control_output(void)
 {
-	Str_out_point *outs;
-	U8_T point = 0;
-	U32_T val;
-//	U8_T loop;
-	U32_T value;
+	Str_out_point* _outputs;
+	uint8_t point = 0;
+	uint32_t val;
+//	uint8_t loop;
+	uint32_t value;
 	point = 0;
-	outs = outputs;
+	_outputs = outputs;
 
 
-	while( point < MAX_OUTS )
-	{	
-		if(point < get_max_output())
+	while( point < MAX__outputs )
+	{
+		if (point < get_max_output())
 		{			
-			if(point < get_max_internal_output())
+			if (point < get_max_internal_output())
 			{		
-				outs->sub_id = 0;
-				outs->sub_product = 0;
-				outs->sub_number = 0;
-				outs->decom = 0;
+				_outputs->sub_id = 0;
+				_outputs->sub_product = 0;
+				_outputs->sub_number = 0;
+				_outputs->decom = 0;
 			}
-			if( outs->range == not_used_output )
+			if (_outputs->range == not_used_output)
 			{
-				outs->value = 0L;
+				_outputs->value = 0L;
 				val = 0;
 			}
 //			else
 //			{	
-//					if( outs->digital_analog == 1 ) //  analog
-//					{	
-//						if(point >= get_max_internal_output())  // // external ouput 					
-//						{  // range is 0-10v
-//							if(outs->sub_product != 44/*PM_T38AI8AO6DO*/)
+//				if ( _outputs->digital_analog == 1 ) //  analog
+//				{	
+//					if (point >= get_max_internal_output())  // // external ouput 					
+//					{  // range is 0-10v
+//						if (_outputs->sub_product != 44/*PM_T38AI8AO6DO*/)
+//						{
+//							if (_outputs->read_remote == 1)
 //							{
-//								if(outs->read_remote == 1)
+//								val = 10000000 / 4095;
+//								val = val * get_output_raw(point) / 1000;
+//								_outputs->read_remote = 0;
+//							}
+//							else
+//							{		
+//								val = swap_double(_outputs->value);	
+//	//							val = val * 4095 / 10000;
+//								// ?????????
+//								if (val < get_output_raw(point))
 //								{
-//									val = 10000000 / 4095;
-//									val = val * get_output_raw(point) / 1000;
-//									outs->read_remote = 0;
+//									if (get_output_raw(point) - val == 1)
+//										set_output_raw(point,val + 1);//output_raw[point] = val + 1;
+//									if (get_output_raw(point) - val == 2)
+//										set_output_raw(point,val + 2);//output_raw[point] = val + 2;
 //								}
 //								else
-//								{		
-//									val = swap_double(outs->value);	
-//	//								val = val * 4095 / 10000;
-//									// ?????????
-//									if(val < get_output_raw(point))
-//									{
-//										if(get_output_raw(point) - val == 1)
-//											set_output_raw(point,val + 1);//output_raw[point] = val + 1;
-//										if(get_output_raw(point) - val == 2)
-//											set_output_raw(point,val + 2);//output_raw[point] = val + 2;
-//									}
-//									else
-//										set_output_raw(point,val);//output_raw[point] = val;
-//									
-//								}
+//									set_output_raw(point,val);//output_raw[point] = val;
+//								
 //							}
 //						}
-////					}
+//					}
 //				}
 //			}
+//		}
 
-			if(check_external_out_on_line(point) == 1)
+			if (check_external_out_on_line(point) == 1)
 			{	
 				map_extern_output(point);
 			}
 			else
 			{
-				outs->sub_id = 0;
-				outs->sub_product = 0;
-				outs->sub_number = 0;
-				
-			  if(point >= get_max_internal_output())
-					outs->decom = 1;  // no used
+				_outputs->sub_id = 0;
+				_outputs->sub_product = 0;
+				_outputs->sub_number = 0;
+			
+				if (point >= get_max_internal_output())
+				{
+					_outputs->decom = 1;  // no used
+				}
 			}
-
 		}
 		else
 		{
-			outs->sub_id = 0;
-			outs->sub_product = 0;
-			outs->sub_number = 0;
-			
-
-			outs->decom = 1;  // no used
+			_outputs->sub_id = 0;
+			_outputs->sub_product = 0;
+			_outputs->sub_number = 0;
+	
+			_outputs->decom = 1;  // no used
 		}
 		point++;
-		outs++;
-				
-	}	
-
+		_outputs++;
+	}
 }
-
 #endif
-
